499e22e6bad83e46e0b819b5ae6bae25277697b3
Старков Виталий <vetalsta@gmail.com>
2024-02-13 17:16:45 +0300

    without csv

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'titles', TitlesViewSet, basename='title')
+router.register(r'genres', GenresViewSet, basename='genre')
+router.register(r'categories', CategoriesViewSet, basename='categorie')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
